7c4bce56348855003e668efecd5b118989716979
Xin Deng <xind1470@gmail.com>
2023-12-11 15:53:32 -0800

    first commit with files

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/test', (request, response) => {
 
