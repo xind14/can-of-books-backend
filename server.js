a4085e5e1eed6e4cefeeefd625d6baa91d0ce5e0
JohnnyBackus <john.p.backus@gmail.com>
2023-12-12 17:15:12 -0800

    server now can post and delete

--- server.js
+++ server.js
@@ -3,29 +3,28 @@
 // require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const handleGetBooks = require('./handlers/getbooks.js')
+const handlePostBooks = require('./handlers/postbooks.js')
 const Books = require('./models/books.js');
+const handleDeleteBooks = require('./handlers/deletebooks.js');
 
 const app = express();
 app.use(cors());
-
-
-
+app.use(express.json());
 
 app.get ('/books', handleGetBooks);
+app.get('/', (request, response) => {
+  response.send('home page')
+})
+app.get('/test', (request, response) => {
+  response.send('test request received')
+})
+app.get('*', (request, response) => {
+  response.status(404).send('Not available');
+});
+
 app.post ('/books', handlePostBooks);
-
-async function handleGetBooks (request, response){
-  let filter = {};
-  const books = await Books.find(filter)
-  response.status(200).json(books);
-}
-
-async function handlePostBooks (request, response){
-  let filter = {};
-  const books = await Books.find(filter)
-  response.status(200).json(books);
-}
+app.delete ('/books/:id', handleDeleteBooks)
 
 const server = {
   start: function(PORT){
@@ -33,16 +32,6 @@ const server = {
 
   }
 }
-
-app.get('*', (request, response) => {
-  response.status(404).send('Not available');
-});
-
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
 
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
