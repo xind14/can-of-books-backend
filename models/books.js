68b95164506a9ef0e2f63ed38b223bab19eb4b09
Xin Deng <xind1470@gmail.com>
2023-12-12 18:30:47 -0800

    saving what we have

--- models/books.js
+++ models/books.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: String,
   description: String,
-  status: Boolean,
+  status: String,
 })
 
 const Book = mongoose.model('Book', bookSchema);
